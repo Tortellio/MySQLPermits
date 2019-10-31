128e5668cb235e0ab1896161b83c50fef5f51fbc
Irfan Miftah <irfan_miftah51@yahoo.com>
2019-10-31 22:15:39 +0700

    Fix for Harbor Build

---
+++ DatabaseManager.cs
@@ -0,0 +1,147 @@
+﻿using MySql.Data.MySqlClient;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Anomoly.MySQLPermits
+{
+    public class DatabaseManager
+    {
+        public DatabaseManager()
+        {
+            new I18N.West.CP1250();
+            CheckSchema();
+        }
+
+        private MySqlConnection CreateConnection()
+        {
+            MySqlConnection connection = null;
+            try
+            {
+                if (MySQLPermitsPlugin.Instance.Configuration.Instance.DatabasePort == 0) MySQLPermitsPlugin.Instance.Configuration.Instance.DatabasePort = 3306;
+                connection = new MySqlConnection(String.Format("SERVER={0};DATABASE={1};UID={2};PASSWORD={3};PORT={4};", MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseAddress, MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseName, MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseUsername, MySQLPermitsPlugin.Instance.Configuration.Instance.DatabasePassword, MySQLPermitsPlugin.Instance.Configuration.Instance.DatabasePort));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+            return connection;
+        }
+
+        public bool HasPermit(string steamId, string permit)
+        {
+            try
+            {
+                MySqlConnection connection = CreateConnection();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "select 1 from `" + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + "` where `steamId` = '" + steamId + "' and `permit_name` = '" + permit + "';";
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null) return true;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+            return false;
+        }
+
+        public bool HasPermit(string steamid)
+        {
+            try
+            {
+                MySqlConnection c = CreateConnection();
+                MySqlCommand command = c.CreateCommand();
+                command.CommandText = "SELECT * FROM " + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + " WHERE `steamId` = '" + steamid + "'";
+                c.Open();
+                object result = command.ExecuteScalar();
+                if (result != null) return true;
+                c.Clone();
+            }
+            catch(Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+            return false;
+        }
+
+        public void AddPermit(UnturnedPlayer player, string Permit)
+        {
+            try
+            {
+                MySqlConnection c = CreateConnection();
+                MySqlCommand command = c.CreateCommand();
+                command.CommandText = "INSERT INTO " + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + " (`steamId`,`permit_name`) VALUES('" + player.Id + "','" + Permit + "')";
+                c.Open();
+                command.ExecuteNonQuery();
+                c.Close();
+            }
+            catch(Exception ex) { Logger.LogException(ex); }
+        }
+
+        public void RemovePermit(UnturnedPlayer player, string permit)
+        {
+            try
+            {
+                MySqlConnection c = CreateConnection();
+                MySqlCommand command = c.CreateCommand();
+                command.CommandText = "DELETE FROM " + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + " WHERE `steamId` = '" + player.Id + "' AND `permit_name` = '" + permit + "'";
+                c.Open();
+                command.ExecuteNonQuery();
+                c.Close();
+            }
+            catch (Exception ex) { Logger.LogException(ex); }
+        }
+
+        public void CheckSchema()
+        {
+            try
+            {
+                MySqlConnection connection = CreateConnection();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "show tables like '" + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + "'";
+                connection.Open();
+                object test = command.ExecuteScalar();
+
+                if (test == null)
+                {
+                    command.CommandText = "CREATE TABLE `" + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + "` (`id` INT NOT NULL AUTO_INCREMENT , `steamId` VARCHAR(35) NOT NULL , `permit_name` VARCHAR(55) NOT NULL , `permit_duration` INT NOT NULL DEFAULT '0' , `permit_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() , PRIMARY KEY (`id`));";
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
+
+        public List<string> GetPlayerPermits(UnturnedPlayer player)
+        {
+            List<string> output = new List<string>();
+            try
+            {
+                MySqlConnection c = CreateConnection();
+                MySqlCommand command = c.CreateCommand();
+                command.CommandText = "SELECT * FROM " + MySQLPermitsPlugin.Instance.Configuration.Instance.DatabaseTableName + " WHERE `steamId` = '" + player.CSteamID + "'";
+                c.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string permit = reader.GetString(2);
+                    output.Add(permit);
+                }
+                c.Close();
+            }
+            catch(Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+            return output;
+        }
+    }
+}
---
+++ MySQLPermitsPlugin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Core.Plugins;
+using Steamworks;
+using UnityEngine;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+using Rocket.API.Collections;
+using System.Timers;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace Anomoly.MySQLPermits
+{
+    public class MySQLPermitsPlugin : RocketPlugin<PermitsConfiguration>
+    {
+        public static MySQLPermitsPlugin Instance { get; private set; }
+        public DatabaseManager DatabaseManager { get; private set; }
+        protected override void Load()
+        {
+            Instance = this;
+            try
+            {
+                DatabaseManager = new DatabaseManager();
+            }
+            catch(Exception ex) { Logger.LogException(ex); }
+            
+            Logger.Log("MySQLPermits by Anomoly has loaded!");
+        }
+
+        protected override void Unload()
+        {
+            DatabaseManager = null;
+            Instance = null;
+            Logger.Log("MySQLPermits by Anomoly has unloaded!");
+        }
+
+        #region Utils
+
+        public bool IsValidPermitName(string name)
+        {
+            foreach(PermitModel p in Configuration.Instance.Permits)
+            {
+                if(p.PermitName.ToLower() == name.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string GetPermit(string name)
+        {
+            PermitModel p = Configuration.Instance.Permits.Find(x => x.PermitName.ToLower() == name.ToLower());
+            if(p == null)
+            {
+                return string.Empty;
+            }
+            return p.PermitName;
+        }
+
+        #endregion
+
+        public override TranslationList DefaultTranslations => 
+            new TranslationList() {
+                    {"command_permits_list","You have the following permits: {0}!"},
+                    {"comamnd_permit_add_alreadyHasPermit","{0} already has the {1} permit!" },
+                    {"command_permit_add","You have given {0} a {1} permit!" },
+                    {"command_permit_remove_DoesNotHavePermit","{0} does not have the {1} permit!" },
+                    {"command_permit_remove","You have removed the {0} permit from {1}!" },
+                    {"command_permit_add_remove_permitNotFound","Permit not found!" },
+                    {"comamnd_permit_playerNotFound","Player not found!" },
+                    {"command_permit_check_hasNotPermtis","{0} does not have any permits" },
+                    {"command_permit_check_List","{0} has the following permits: {1}!" },
+                    {"command_permit_NoPermissions","You do not have permission to use this part of the command!" },
+                    {"command_permit_admin_usage","Please do /permit [add|remove|check] <player> [permit]" },
+                    {"command_permits_usage","Please do /permit to check your current permits!" }
+                };
+    }
+}
